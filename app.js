e1eb47c3cc4b3aa52b5dddf542cd1d826b06ccf1
Yuriy Soproniuk <sopronukCC@gmail.com>
2021-02-24 21:10:01 +0200

    routes

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const Koa = require('koa');
 const path = require('path');
-const Router = require('koa-router');
 
 const app = new Koa();
-const router = new Router({ prefix: '/api' });
 
 
 app.use(require('koa-bodyparser')());
@@ -11,7 +9,6 @@ app.use(require('koa-static')(path.join(__dirname, 'public')));
 
 app.use(require('./middlewares/error'));
 
-
-app.use(router.routes());
+app.use(require('./router').routes());
 
 module.exports = app;
